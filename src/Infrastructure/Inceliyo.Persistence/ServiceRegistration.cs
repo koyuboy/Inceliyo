7562424f61761ca676f35af52cdb1f0022963888
Ahmet Huzeyfe Demir <35155252+koyuboy@users.noreply.github.com>
2022-05-26 17:04:31 +0300

    Add project files.

---
+++ src/Infrastructure/Inceliyo.Persistence/ServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inceliyo.Persistence
+{
+    public static class ServiceRegistration
+    {
+    }
+}
