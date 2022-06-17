644738300decb287a4f895bae4ce4a35eb651224
Ahmet Huzeyfe Demir <35155252+koyuboy@users.noreply.github.com>
2022-06-17 22:31:50 +0300

    Added IGenericRepository * implemented as GenericRepository

---
+++ src/Infrastructure/Inceliyo.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,81 @@
+﻿using Inceliyo.Application.Interfaces.Repositories;
+using Inceliyo.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inceliyo.Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        
+        
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.AnyAsync(expression);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet.AsNoTracking().AsQueryable();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+            
+            _context.SaveChanges();
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+
+            _context.SaveChanges();
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Where(expression);
+        }
+    }
+}
---
+++ test/TestGenericRepository/Program.cs
@@ -0,0 +1,71 @@
+﻿using Inceliyo.Domain.Entities;
+using Inceliyo.Persistence.Contexts;
+using Inceliyo.Persistence.Repositories;
+
+namespace TestGenericRepository
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            GenericRepository<Category> gp = new GenericRepository<Category>(new AppDbContext());
+
+            
+
+            var resultGetAll = gp.GetAll().ToList();
+            Console.WriteLine($"GetAll: {resultGetAll[0].Name}\n\n");
+
+            var resultGetByIdAsync = await gp.GetByIdAsync(Guid.Parse("FFBF052B-833E-47D8-925F-9CBE83623347"));
+            Console.WriteLine($"GetByIdAsync: {resultGetByIdAsync.Name}\n\n");
+
+            var resultWhere = gp.Where(x=> x.Name == "Category 272128").ToList();
+            Console.WriteLine($"Where: {resultWhere[0].Name}\n\n");
+
+            var resultAnyAsync = await gp.AnyAsync(x => x.Name == "Category 272128");
+            Console.WriteLine($"AnyAsync: {resultAnyAsync}\n\n");
+
+            var AddCategoryItem = new Category()
+            {
+                Name = "Category " + DateTime.Now.ToString("dd/HH:mm"),
+                ParentId = Guid.NewGuid()
+            };
+            await gp.AddAsync(AddCategoryItem);
+            Console.WriteLine($"AddAsync: DONE \n\n");
+
+
+            List<Category> AddRangeCategoriesItems = new List<Category>(); ;
+            AddRangeCategoriesItems.Add(new Category()
+            {
+                Name = "CategoryRange " + DateTime.Now.ToString("dd/HH:mm"),
+                ParentId = Guid.NewGuid()
+            });
+            AddRangeCategoriesItems.Add(new Category()
+            {
+                Name = "CategoryRange " + DateTime.Now.ToString("dd/HH:mm"),
+                ParentId = Guid.NewGuid()
+            });
+
+            await gp.AddRangeAsync(AddRangeCategoriesItems);
+            Console.WriteLine($"AddRangeAsync: DONE \n\n");
+
+
+            Console.WriteLine("press key to update data \n\n");
+            Console.ReadLine();
+
+            AddCategoryItem.Name = "New_" + AddCategoryItem.Name;
+            gp.Update(AddCategoryItem);
+            Console.WriteLine($"Update: DONE \n\n");
+
+            Console.WriteLine("press key to delete added data \n\n");
+            Console.ReadLine();
+
+            gp.Remove(AddCategoryItem);
+            Console.WriteLine($"Remove: DONE \n\n");
+
+            gp.RemoveRange(AddRangeCategoriesItems);
+            Console.WriteLine($"RemoveRange: DONE \n\n");
+
+            Console.ReadLine();
+        }
+    }
+}
